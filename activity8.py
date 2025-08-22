e6437040afb6c7922225c54bf1a2ca1e10222943
Chrysler Anne Idea <chryslerannei@gmail.com>
2025-08-22 16:44:52 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,8 @@
+a = 26
+b = 24
+
+name1 = 'Chrysler'
+name2 = 'Leria'
+
+print(a <= b)
+print(name1 <= name2)
---
+++ activity9.py
@@ -0,0 +1,13 @@
+a = 6
+b = 4
+
+c = 8
+d = 7
+
+username = 'Chryslrnnd'
+password = 'poginya2624'
+
+print("USER LOGIN")
+print( (username == 'Chryslrnnd') and (password == 'poginya2624'))
+
+print( not((a < b) or (c < d)) )
